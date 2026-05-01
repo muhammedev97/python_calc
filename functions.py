d44641cef04d47df7a88dc0094c3c5c8520487ab
Zaur Hasanli <zaurhasanli98@gmail.com>
2026-05-01 11:09:04 +0400

    some changes

--- functions.py
+++ functions.py
@@ -1,55 +1,72 @@
-def sum(a, b):
-    return a + b
+# Bu funksiyalar riyazi hesablamaları yerinə yetirmək üçündür.
+def topla(a, b):  # Toplama funksiyası
+    netice = a + b  # İki ədədi toplayıb dəyişənə yazırıq
+    return netice  # Cavabı geri qaytarırıq
 
-def sub(a, b):
-    return a - b
+def cix(a, b):  # Çıxma funksiyası
+    netice = a - b  # Birinci ədəddən ikincini çıxırıq
+    return netice  # Nəticəni qaytarırıq
 
-def mul(a, b):
-    return a * b
+def vur(a, b):  # Vurma funksiyası
+    netice = a * b  # Ədədləri bir-birinə vururuq
+    return netice  # Cavabı geri veririk
 
-def div(a, b):
-    return a / b
+def bol(a, b):  # Bölmə funksiyası
+    if b == 0:  # Əgər bölən sıfırdırsa
+        raise ZeroDivisionError  # Xəta mesajı fırladırıq (sıfıra bölmək olmaz)
+    netice = a / b  # Bölməni icra edirik
+    return netice  # Nəticəni qaytarırıq
 
+def hesabla_siyahini(siyahi):  # Siyahıdakı ifadəni hesablayan əsas hissə
+    is_siyahisi = siyahi.copy()  # Orijinal siyahı korlanmasın deyə kopya çıxarırıq
 
-def sort(args):
-    i = 0
-    while i < len(args):
-        if args[i] == "*" or args[i] == "/":
-            left = float(args[i - 1])
-            right = float(args[i + 1])
+    i = 0  # Dövr üçün indeks təyin edirik
+    while i < len(is_siyahisi):  # Siyahının sonuna qədər gedirik
+        element = is_siyahisi[i]  # Cari elementi götürürük
+        if element == "-" and (i == 0 or is_siyahisi[i-1] in ["+", "-", "*", "/"]):  # Mənfi işarəsini yoxlayırıq
+            if i + 1 < len(is_siyahisi):  # İşarədən sonra nəsə varmı?
+                menfi_eded = float(is_siyahisi[i+1]) * -1  # Ədədi mənfiyə vurub mənfi edirik
+                is_siyahisi[i : i+2] = [menfi_eded]  # İşarə və rəqəmi tək mənfi ədədlə əvəzləyirik
+                i = 0  # Siyahı dəyişdi, yenidən başdan başlayırıq
+                continue  # Növbəti dövrə keçirik
+        i = i + 1  # İndeksi bir vahid artırırıq
 
-            if args[i] == "*":
-                result = mul(left, right)
-            else:
-                result = div(left, right)
+    i = 0  # İndeksi yenidən sıfırlayırıq
+    while i < len(is_siyahisi):  # Vurma və bölmələri axtarırıq
+        if is_siyahisi[i] == "*" or is_siyahisi[i] == "/":  # Əgər vurma və ya bölmədirsə
+            sol_teref = float(is_siyahisi[i-1])  # Sol tərəfdəki rəqəm
+            sag_teref = float(is_siyahisi[i+1])  # Sağ tərəfdəki rəqəm
+            isare = is_siyahisi[i]  # İşarənin özü
 
-            del args[i - 1:i + 2]
-            args.insert(i - 1, result)
+            if isare == "*":  # Əgər vurmadırsa
+                cavab = vur(sol_teref, sag_teref)  # Vurma funksiyasını çağırırıq
+            else:  # Əgər bölmədirsə
+                cavab = bol(sol_teref, sag_teref)  # Bölmə funksiyasını çağırırıq
+            
+            is_siyahisi[i-1 : i+2] = [cavab]  # Hesabladığımız 3 elementi tək cavabla əvəz edirik
+            i = 0  # Başdan yenidən yoxlayırıq
+        else:  # Əgər vurma/bölmə deyilsə
+            i = i + 1  # Növbəti elementə keçirik
 
-            i = 0
-        else:
-            i += 1
+    i = 0  # İndeksi axırıncı dəfə sıfırlayırıq
+    while i < len(is_siyahisi):  # İndi isə toplama və çıxmaları tapırıq
+        if is_siyahisi[i] == "+" or is_siyahisi[i] == "-":  # Əgər toplama və ya çıxmadırsa
+            sol_teref = float(is_siyahisi[i-1])  # Sol arqument
+            sag_teref = float(is_siyahisi[i+1])  # Sağ arqument
+            isare = is_siyahisi[i]  # Hansı işarədir?
 
-    i = 0
-    while i < len(args):
-        if args[i] == "+" or args[i] == "-":
-            left = float(args[i - 1])
-            right = float(args[i + 1])
+            if isare == "+":  # Toplamadırsa
+                cavab = topla(sol_teref, sag_teref)  # Toplayırıq
+            else:  # Çıxmadırsa
+                cavab = cix(sol_teref, sag_teref)  # Çıxırıq
+            
+            is_siyahisi[i-1 : i+2] = [cavab]  # Nəticəni siyahıya yazırıq
+            i = 0  # Yenidən başa qayıdırıq
+        else:  # Başqa bir şeydirsə
+            i = i + 1  # İrəli gedirik
 
-            if args[i] == "+":
-                result = sum(left, right)
-            else:
-                result = sub(left, right)
+    return is_siyahisi  # Hesablanmış siyahını geri qaytarırıq
 
-            del args[i - 1:i + 2]
-            args.insert(i - 1, result)
-
-            i = 0
-        else:
-            i += 1
-
-    return args
-
-
-def calculate(args):
-    return sort(args)
+def calculate(args):  # Main tərəfindən çağırılan funksiya
+    netice_listi = hesabla_siyahini(args)  # Hesablamanı başladırıq
+    return netice_listi  # Nəticəni main-ə veririk
--- main.py
+++ main.py
@@ -1,137 +1,119 @@
-import tkinter as tk
-import functions as f
+import tkinter as tk  # İnterfeys üçün kitabxana
+import functions as f  # Hesablama funksiyalarımızı gətiririk
 
-window = tk.Tk()
-window.title("Zaur & Muhammed Kalkulyator")
-window.geometry("300x400")
-window.resizable(False, False)
+pencere = tk.Tk()  # Proqramın əsas pəncərəsini yaradırıq
+pencere.title("Zaur & Mehemmed Kalkulyatoru")  # Pəncərənin başlığını qoyuruq
+pencere.geometry("300x400")  # Ölçülərini təyin edirik
+pencere.resizable(False, False)  # Ölçüsünün dəyişdirilməsini qadağan edirik
 
-val = tk.StringVar()
-expression = ""
-expression_args = []
+ekran_yazisi = tk.StringVar()  # Ekrandakı yazını idarə edən dəyişən
+ifade_metni = ""  # Yazılan rəqəmləri sətir kimi saxlayırıq
+ifade_listi = []  # Hesablamaq üçün rəqəm və işarələri siyahı edirik
 
+def duymeye_basanda(acar):  # Rəqəm və işarə düymələri üçün funksiya
+    global ifade_metni  # Global dəyişəni istifadə edirik
+    if acar.isdigit():  # Əgər basılan simvol rəqəmdirsə
+        ifade_metni = ifade_metni + acar  # Yazıya rəqəmi artırırıq
+        ekran_yazisi.set(ifade_metni)  # Ekranda göstəririk
+        if ifade_listi and str(ifade_listi[-1]).replace(".", "", 1).isdigit():  # Əvvəlki də rəqəmdirsə
+            ifade_listi[-1] = ifade_listi[-1] + acar  # Onları birləşdiririk (məsələn 1 və 2 olur 12)
+        else:  # Yeni bir ədəddirsə
+            ifade_listi.append(acar)  # Siyahıya təzə element kimi əlavə edirik
+    elif acar == ".":  # Nöqtə düyməsi basılıbsa
+        if not ifade_listi or not str(ifade_listi[-1]).replace(".", "", 1).isdigit():  # Boşdursa
+            ifade_metni = ifade_metni + "0."  # "0." yazırıq
+            ifade_listi.append("0.")  # Siyahıya əlavə edirik
+        elif "." not in ifade_listi[-1]:  # Əgər ədəddə hələ nöqtə yoxdursa
+            ifade_metni = ifade_metni + "."  # Nöqtəni artırırıq
+            ifade_listi[-1] = ifade_listi[-1] + "."  # Siyahıda da qeyd edirik
+        ekran_yazisi.set(ifade_metni)  # Ekranı yeniləyirik
+    elif acar in ["+", "*", "/"]:  # Əməliyyat düymələri
+        if len(ifade_listi) > 0:  # Əgər ekranda nəsə varsa
+            if ifade_listi[-1] in ["+", "-", "*", "/"]:  # Axırıncı simvol artıq işarədirsə
+                ifade_listi[-1] = acar  # Köhnə işarəni silib təzəsini qoyuruq
+                ifade_metni = ifade_metni[:-1] + acar  # Mətndə də dəyişirik
+            else:  # Normal halda
+                ifade_listi.append(acar)  # İşarəni siyahıya atırıq
+                ifade_metni = ifade_metni + acar  # Mətnə artırırıq
+        ekran_yazisi.set(ifade_metni)  # Ekranda göstəririk
+    elif acar == "-":  # Çıxma və ya mənfi işarəsi
+        if not ifade_listi:  # Siyahı boşdursa
+            ifade_metni = ifade_metni + "-"  # Mənfi kimi başlayırıq
+            ifade_listi.append("-")  # Siyahıya mənfi atırıq
+        elif ifade_listi[-1] in ["+", "-", "*", "/"]:  # İşarədən sonra gəlirsə
+            ifade_metni = ifade_metni + "-"  # Yenə mənfi rəqəm üçün
+            ifade_listi.append("-")  # Siyahıya əlavə
+        else:  # Normal çıxma əməliyyatı
+            ifade_listi.append("-")  # Siyahıya çıxma işarəsi
+            ifade_metni = ifade_metni + "-"  # Mətnə çıxma
+        ekran_yazisi.set(ifade_metni)  # Ekranı yeniləyirik
 
-def press(key):
-    global expression
-    expression += str(key)
-    val.set(expression)
+def hamisini_sil():  # C düyməsinin funksiyası
+    global ifade_metni  # Mətni sıfırlamaq üçün
+    ifade_metni = ""  # Boş sətir edirik
+    ifade_listi.clear()  # Siyahını təmizləyirik
+    ekran_yazisi.set("")  # Ekranı boşaldırıq
 
-    if str(key).isdigit():
-        if len(expression_args) > 0 and str(expression_args[-1]).replace(".", "", 1).isdigit():
-            expression_args[-1] = str(expression_args[-1]) + str(key)
-        else:
-            expression_args.append(str(key))
+def sonuncunu_sil():  # CE düyməsinin funksiyası
+    global ifade_metni  # Mətni dəyişmək üçün
+    if len(ifade_listi) > 0:  # Siyahı boş deyilsə
+        silinen = ifade_listi.pop()  # Sonuncu elementi siyahıdan çıxarırıq
+        uzunluq = len(str(silinen))  # Həmin elementin neçə hərfdən ibarət olduğunu tapırıq
+        ifade_metni = ifade_metni[:-uzunluq]  # Mətndən o qədər simvol silirik
+        ekran_yazisi.set(ifade_metni)  # Ekranı yeniləyirik
 
-    elif key == ".":
-        if len(expression_args) == 0:
-            expression += "0"
-            expression_args.append("0.")
-        elif str(expression_args[-1]).replace(".", "", 1).isdigit():
-            if "." not in str(expression_args[-1]):
-                expression_args[-1] = str(expression_args[-1]) + "."
-        else:
-            expression_args.append("0.")
-
-    elif key in ["+", "-", "*", "/"]:
-        if len(expression_args) == 0:
-            return
-
-        if expression_args[-1] in ["+", "-", "*", "/"]:
-            expression_args[-1] = key
-            expression = expression[:-2] + key
-            val.set(expression)
-        else:
-            expression_args.append(key)
-
-
-def clear():
-    global expression
-    expression = ""
-    val.set("")
-    expression_args.clear()
-
-
-def backspace():
-    global expression
-
-    if len(expression) > 0:
-        expression = expression[:-1]
-        val.set(expression)
-
-    if len(expression_args) > 0:
-        last = str(expression_args[-1])
-
-        if len(last) > 1 and last not in ["+", "-", "*", "/"]:
-            expression_args[-1] = last[:-1]
-        else:
-            expression_args.pop()
-
-
-def percent():
-    global expression
-
-    if len(expression_args) == 0:
+def bir_simvol_sil():  # Backspace funksiyası
+    global ifade_metni  # Global mətni götürürük
+    if ifade_metni == "":  # Əgər onsuz da boşdursa
+        return  # Heç nə etmirik
+    ifade_metni = ifade_metni[:-1]  # Axırıncı bir hərfi silirik
+    ekran_yazisi.set(ifade_metni)  # Ekranda göstəririk
+    if not ifade_listi:  # Siyahı boşdursa çıxırıq
         return
+    axirinci = ifade_listi[-1]  # Sonuncu elementə baxırıq
+    if axirinci in ["+", "-", "*", "/"]:  # İşarədirsə
+        ifade_listi.pop()  # Tamamilə silirik
+    else:  # Rəqəmdirsə
+        if len(axirinci) > 1:  # Birdən çox rəqəmdən ibarətdirsə
+            ifade_listi[-1] = axirinci[:-1]  # Birini silirik
+        else:  # Tək rəqəmdirsə
+            ifade_listi.pop()  # Siyahıdan çıxarırıq
 
-    if expression_args[-1] in ["+", "-", "*", "/"]:
-        return
+def faiz_hesabla():  # % düyməsinin funksiyası
+    global ifade_metni  # Mətni yeniləmək üçün
+    if ifade_listi:  # Siyahı boş deyilsə
+        if ifade_listi[-1] not in ["+", "-", "*", "/"]:  # Axırıncı rəqəmdirsə
+            reqem = float(ifade_listi[-1]) / 100  # 100-ə bölüb faiz edirik
+            ifade_listi[-1] = str(reqem)  # Siyahıda yeniləyirik
+            ifade_metni = str(reqem)  # Ekranda göstərmək üçün metni dəyişirik
+            ekran_yazisi.set(ifade_metni)  # Ekranı yeniləyirik
 
-    value = float(expression_args[-1]) / 100
-    expression_args[-1] = str(value)
-
-    parts = expression.split()
-    expression = expression[:-len(str(parts[-1]))] + str(value) if parts else str(value)
-    val.set(expression)
-
-
-def calculate():
-    global expression
-
+def hesabla_duymesi():  # = düyməsi üçün funksiya
+    global ifade_metni  # Nəticəni ekrana yazmaq üçün
     try:
-        if len(expression_args) == 0:
+        if not ifade_listi:  # Boşdursa hesablamırıq
             return
-
-        if expression_args[-1] in ["+", "-", "*", "/"]:
+        if ifade_listi[-1] in ["+", "-", "*", "/"]:  # Axırı işarə ilə bitibsə olmaz
             return
+        sonuc_listi = f.calculate(ifade_listi.copy())  # Hesablama funksiyasını çağırırıq
+        cavab = sonuc_listi[0]  # Gələn cavabı götürürük
+        if cavab == int(cavab):  # Əgər tam ədəddirsə (.0 yoxdursa)
+            cavab = int(cavab)  # Tam ədədə çeviririk
+        ifade_metni = str(cavab)  # Nəticəni mətnə çeviririk
+        ifade_listi.clear()  # Siyahını təmizləyirik
+        ifade_listi.append(str(cavab))  # Cavabı siyahıya təzə başlanğıc kimi qoyuruq
+        ekran_yazisi.set(ifade_metni)  # Ekranda cavabı göstəririk
+    except ZeroDivisionError:  # Sıfıra böləndə
+        ekran_yazisi.set("Sıfıra bölmək olmaz!")  # Xəbərdarlıq yazırıq
+        hamisini_sil()  # Ekranı təmizləyirik
+    except:  # Başqa bir xəta olsa
+        ekran_yazisi.set("Xəta baş verdi")  # Xəta mesajı
+        hamisini_sil()  # Təmizləyirik
 
-        result = f.calculate(expression_args)[0]
+ekran = tk.Entry(pencere, textvariable=ekran_yazisi, justify="right", font=("Arial", 20))  # Yazı sahəsi
+ekran.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)  # Ekrana yerləşdiririk
 
-        if result == int(result):
-            result = int(result)
-
-        expression = str(result)
-        expression_args.clear()
-        expression_args.append(str(result))
-        val.set(expression)
-
-    except ZeroDivisionError:
-        val.set("Sıfıra bölünmez")
-        expression = ""
-        expression_args.clear()
-
-    except Exception:
-        val.set("Hata")
-        expression = ""
-        expression_args.clear()
-
-
-input_field = tk.Entry(
-    window,
-    textvariable=val,
-    justify="right",
-    font=("Arial", 18, "bold")
-)
-input_field.config(state="readonly")
-input_field.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=10, pady=10)
-
-for i in range(4):
-    window.grid_columnconfigure(i, weight=1)
-
-for i in range(6):
-    window.rowconfigure(i, weight=1, uniform="row")
-
-
-buttons = [
+duymeler = [  # Düymələrin siyahısı
     ("%", 1, 0), ("CE", 1, 1), ("C", 1, 2), ("⌫", 1, 3),
     ("7", 2, 0), ("8", 2, 1), ("9", 2, 2), ("/", 2, 3),
     ("4", 3, 0), ("5", 3, 1), ("6", 3, 2), ("*", 3, 3),
@@ -139,27 +121,27 @@ buttons = [
     ("0", 5, 0), (".", 5, 1), ("=", 5, 2), ("+", 5, 3)
 ]
 
+for (yazi, setir, sutun) in duymeler:  # Hər bir düymə üçün dövr
+    if yazi == "=":  # Əgər bərabərdirsə
+        komanda = hesabla_duymesi  # Hesabla funksiyasını bağlayırıq
+    elif yazi == "C":  # Əgər C düyməsidirsə
+        komanda = hamisini_sil  # Həmişini sil funksiyası
+    elif yazi == "CE":  # Əgər CE-dirsə
+        komanda = sonuncunu_sil  # Sonuncunu sil funksiyası
+    elif yazi == "⌫":  # Əgər silmə işarəsidirsə
+        komanda = bir_simvol_sil  # Bir simvol sil funksiyası
+    elif yazi == "%":  # Əgər faizdirsə
+        komanda = faiz_hesabla  # Faiz funksiyası
+    else:  # Rəqəm və ya digər işarələrdirsə
+        komanda = lambda t=yazi: duymeye_basanda(t)  # Düyməyə basma funksiyası
 
-for (text, row, col) in buttons:
-    if text == "=":
-        action = calculate
-    elif text == "C":
-        action = clear
-    elif text == "CE":
-        action = clear
-    elif text == "⌫":
-        action = backspace
-    elif text == "%":
-        action = percent
-    else:
-        action = lambda t=text: press(t)
+    # Düyməni yaradırıq
+    tk.Button(pencere, text=yazi, command=komanda, font=("Arial", 14)).grid(row=setir, column=sutun, sticky="nsew", padx=2, pady=2)
 
-    tk.Button(
-        window,
-        text=text,
-        command=action,
-        font=("Arial", 14)
-    ).grid(row=row, column=col, sticky="nsew", padx=2, pady=2)
+for i in range(4):  # Sütunları nizamlamaq üçün dövr
+    pencere.grid_columnconfigure(i, weight=1)  # Hər sütuna eyni çəki veririk
 
+for i in range(6):  # Sətirləri nizamlamaq üçün dövr
+    pencere.grid_rowconfigure(i, weight=1)  # Hər sətirə eyni çəki veririk
 
-window.mainloop()
+pencere.mainloop()  # Proqramı işlək vəziyyətdə saxlayırıq
